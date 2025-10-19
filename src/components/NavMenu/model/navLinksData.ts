065568c4f46c71bdcd6bf08eb4e5bf293fa248ec
Ivan <vany4golos@yandex.ru>
2025-10-19 15:55:59 +0300

    feat: burgerMenu

---
+++ src/components/NavMenu/model/navLinksData.ts
@@ -0,0 +1,23 @@
+interface INavLinksData {
+  label: string;
+}
+
+export const navLinksData: INavLinksData[] = [
+  {
+    label: 'Главная',
+  },
+  { 
+    label: 'Оборудования',
+  },
+  { 
+    label: 'Материалы',
+  },
+  { 
+    label: 'О компании',
+  },
+  { 
+    label: 'Контакты',
+  },
+];
+
+// Контакты
